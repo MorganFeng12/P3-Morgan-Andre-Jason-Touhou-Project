4fb490bbf824678eb4ed56599c28094b8990da56
jasonhu27 <60557121+jasonhu27@users.noreply.github.com>
2021-05-25 13:12:13 -0700

    ber2

--- P3- Morgan, Jason, Andre - Touhou Project/src/Driver.java
+++ P3- Morgan, Jason, Andre - Touhou Project/src/Driver.java
@@ -1,8 +1,58 @@
-public class Driver {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Driver extends JPanel implements ActionListener{
+	//handles drawing animation
+	Timer animationTimer; 
+	Ball b;
+	
+	//int x = 0;
+	
+	
+	public void paint(Graphics g) {
+		super.paintComponent(g);
 		
+		b.paint(g);
+		//g.setColor(Color.blue);
+		//g.fillOval(x, 0, 200, 200);
+		//x += 2;
+		
+		
+	}
+	
+	public Driver() {
+		JFrame f = new JFrame("Example Drawing");
+		f.setSize(800, 600); //width and height
+		
+		//set default action for x button
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+		//add this panel to the JFrame
+		//allows connection with "drawing"
+		f.add(this);
+		
+		//setup animation timer
+		animationTimer = new Timer(16, this);
+		animationTimer.start();
+		
+		//instantiate the rest of the instance variables
+		b = new Ball();
+		
+		f.setVisible(true);
+		
+	}
+
+	/* this method is invoked/called by the timer*/ 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//call the frame to refresh
+		repaint();
 	}
 
 }
