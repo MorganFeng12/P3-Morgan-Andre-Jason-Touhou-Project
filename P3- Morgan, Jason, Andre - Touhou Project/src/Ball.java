dbe5f0d592a6c65c63e1f62ee5229675d95f01c9
jasonhu27 <60557121+jasonhu27@users.noreply.github.com>
2021-06-01 13:37:23 -0700

    Tried uploading gifs, continued setting up the jFrame

--- P3- Morgan, Jason, Andre - Touhou Project/src/Ball.java
+++ P3- Morgan, Jason, Andre - Touhou Project/src/Ball.java
@@ -2,23 +2,27 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Ball {
-	private int x, y; //location
+	private int x, y; // location
 	private int width;
 	private Color c;
-	
+	private int vx, vy; //velocity values
+
 	public Ball() {
-		/* job of the const is to give values to instance variables*/
+		/* job of the const is to give values to instance variables */
 		x = 0;
 		y = 0;
 		width = 50;
 		c = new Color(50, 50, 50);
-	
+
 	}
-	
+
 	public void paint(Graphics g) {
 		g.setColor(this.c);
 		g.fillOval(x, y, width, width);
-		
+
+	}
+
+	public void moveRight() {
 		x += 5;
 	}
 }
--- P3- Morgan, Jason, Andre - Touhou Project/src/Driver.java
+++ P3- Morgan, Jason, Andre - Touhou Project/src/Driver.java
@@ -2,23 +2,24 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class Driver extends JPanel implements ActionListener{
+public class Driver extends JPanel implements ActionListener, KeyListener{
 	//handles drawing animation
 	Timer animationTimer; 
 	Ball b;
-	
-	//int x = 0;
 	
 	
 	public void paint(Graphics g) {
 		super.paintComponent(g);
 		
 		b.paint(g);
+	  	
 		//g.setColor(Color.blue);
 		//g.fillOval(x, 0, 200, 200);
 		//x += 2;
@@ -36,6 +37,8 @@ public class Driver extends JPanel implements ActionListener{
 		//add this panel to the JFrame
 		//allows connection with "drawing"
 		f.add(this);
+		f.addKeyListener(this);
+		
 		
 		//setup animation timer
 		animationTimer = new Timer(16, this);
@@ -50,9 +53,29 @@ public class Driver extends JPanel implements ActionListener{
 
 	/* this method is invoked/called by the timer*/ 
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent arg0) {
 		//call the frame to refresh
 		repaint();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		b.moveRight();
+		
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
