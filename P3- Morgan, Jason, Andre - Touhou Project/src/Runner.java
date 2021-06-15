ca4f6310ee7bc3a586a62fe44d671283d91b56ca
jasonhu27 <60557121+jasonhu27@users.noreply.github.com>
2021-06-15 14:25:46 -0700

    Last

--- P3- Morgan, Jason, Andre - Touhou Project/src/Runner.java
+++ P3- Morgan, Jason, Andre - Touhou Project/src/Runner.java
@@ -1,5 +1,7 @@
 public class Runner {
 	public static void main(String[] args) {
+		//runs the project
+		
 		Driver driver = new Driver();
 
 		try {
--- P3- Morgan, Jason, Andre - Touhou Project/src/Sasuke.java
+++ P3- Morgan, Jason, Andre - Touhou Project/src/Sasuke.java
@@ -1,8 +1,8 @@
 public class Sasuke extends Sprite {
-
+	//reads in the file
 	public Sasuke(String fileName) {
 		super(fileName);
-
+		//setting up all the instance variables
 		width = 150;
 		height = 153;
 		y = 150;
