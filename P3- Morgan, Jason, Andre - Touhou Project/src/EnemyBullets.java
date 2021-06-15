f9ccbaf617e9e25d7f71869c9cbbf14631e08eb1
jasonhu27 <60557121+jasonhu27@users.noreply.github.com>
2021-06-15 14:48:22 -0700

    Finished and Commented Code

--- P3- Morgan, Jason, Andre - Touhou Project/src/EnemyBullets.java
+++ P3- Morgan, Jason, Andre - Touhou Project/src/EnemyBullets.java
@@ -1,15 +1,16 @@
 
 public class EnemyBullets extends Sprite {
-
+	//reads in the file
 	public EnemyBullets(String fileName) {
 		super(fileName);
-
+		//sets all of the instance variables to their right balue
 		width = 25;
 		height = 25;
 		x = 750;
 		y = (int) (Math.random()*700);
-		vx = -8;
-
+	
+		vx = (int) (Math.random()*2 - 7);
+	
 		Enemy = false;
 		bullet = true;
 	}
