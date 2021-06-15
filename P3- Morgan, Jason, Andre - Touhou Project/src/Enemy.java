5f81fa5e1cb99129a22aafc020b189ef1d49616a
jasonhu27 <60557121+jasonhu27@users.noreply.github.com>
2021-06-15 14:47:54 -0700

    Finished and Commented Code

--- P3- Morgan, Jason, Andre - Touhou Project/src/Enemy.java
+++ P3- Morgan, Jason, Andre - Touhou Project/src/Enemy.java
@@ -1,21 +1,23 @@
-
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
 public class Enemy extends Sprite {
-
+	//reads in the file
 	public Enemy(String fileName) {
 		super(fileName);
 		// TODO Auto-generated constructor stub
+		//setting all of the instance variables to the right value
 		vy = 0;
 		vx = -7;
-		x = 750;
+		x = 1000;
 		y = (int) (Math.random() * 450 - 25);
 		width = 150;
 		height = 150;
 		stopDist = (int) (Math.random() * 301 + 425);
 		Enemy = true;
+		bmove = false;
 		bullet = false;
+
 
 	}
 
